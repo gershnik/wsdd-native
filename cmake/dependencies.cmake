bab49d3b9741b28706866c62a8ae0950de7897ab
Eugene Gershnik <gershnik@hotmail.com>
2023-07-29 17:54:53 -0700

    Reverting `fmt` library to 9.1 due to bugs in 10.0

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -74,7 +74,7 @@ set(FMT_INSTALL OFF)
 
 FetchContent_Declare(fmt
     GIT_REPOSITORY  https://github.com/fmtlib/fmt
-    GIT_TAG         10.0.0
+    GIT_TAG         9.1.0
     GIT_SHALLOW     TRUE
     GIT_SUBMODULES_RECURSE FALSE
 )
