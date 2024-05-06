f21178762216bee91e333fd8436d30ff50ac5b80
Eugene Gershnik <gershnik@hotmail.com>
2024-05-05 23:02:29 -0700

    Adding sysv init scripts to debian installer

--- installers/deb-systemd/build.py
+++ installers/deb-systemd/build.py
@@ -36,6 +36,7 @@ installCode(builddir, stagedir / 'usr')
 
 shutil.copytree(srcdir / 'config/systemd/usr', stagedir / 'usr', dirs_exist_ok=True)
 shutil.copytree(srcdir / 'config/systemd/etc', stagedir / 'etc', dirs_exist_ok=True)
+shutil.copytree(srcdir / 'config/sysv/etc',    stagedir / 'etc', dirs_exist_ok=True)
 
 docdir = stagedir / 'usr/share/doc/wsddn'
 docdir.mkdir(parents=True)
